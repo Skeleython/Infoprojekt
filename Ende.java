4d4dedd55354ced9b262873d45be205d5003ec8f
Rasmus Klaukien <rasmus.klaukien@gmail.com>
2020-12-11 11:28:21 +0100

    hi

---
+++ Ende.java
@@ -0,0 +1,33 @@
+
+/**
+ * Beschreiben Sie hier die Klasse Ende.
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class Ende
+{
+    // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
+    private int x;
+
+    /**
+     * Konstruktor für Objekte der Klasse Ende
+     */
+    public Ende()
+    {
+        // Instanzvariable initialisieren
+        x = 0;
+    }
+
+    /**
+     * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
+     * 
+     * @param  y    ein Beispielparameter für eine Methode
+     * @return        die Summe aus x und y
+     */
+    public int beispielMethode(int y)
+    {
+        // tragen Sie hier den Code ein
+        return x + y;
+    }
+}
